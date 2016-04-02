2deac12f1942cf8a0dbeb09745413dfc8d235b38
Peter Lejeck <peter.lejeck@gmail.com>
2016-04-02 00:40:51 -0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,26 +5,34 @@
 
 module Onebox::Engine::HummingbirdOnebox
   include Onebox::Engine
+  include Onebox::JSON
 
   # a|m are short links for anime|manga
   matches_regexp /https?:\/\/(?:www\.)?hummingbird\.me\/(?<type>anime|manga|a|m)\/(?<slug>.+)/
+  always_https
+  
+  def url
+    # TODO: switch to APIv16
+    "https://hummingbird.me/#{type}/#{slug}.json"
+  end
 
   def to_html
     return "<a href=\"#{@url}\">#{@url}</a>" if media.nil?
+
     <<-HTML
       <div class="onebox">
         <div class="source">
           <div class="info">
             <a href="#{@url}" class="track-link" target="_blank">
-              #{media.class.to_s} (#{media_type})
+              #{type} (#{media_type})
             </a>
           </div>
         </div>
         <div class="onebox-body media-embed">
-          <img src="#{media.poster_image.url(:large)}" class="thumbnail">
-          <h3><a href="#{@url}" target="_blank">#{media.canonical_title}</a></h3>
-          <h4>#{media.genres.map {|x| x.name }.sort * ", "}</h4>
-          #{media.synopsis[0..199]}...
+          <img src="#{media['poster_image_thumb']" class="thumbnail">
+          <h3><a href="#{@url}" target="_blank">#{media['romaji_title']}</a></h3>
+          <h4>#{media['genres'].sort * ", "}</h4>
+          #{media['synopsis']}
         </div>
         <div class="clearfix"></div>
       </div>
@@ -34,26 +42,23 @@ module Onebox::Engine::HummingbirdOnebox
   private
 
   def type
-    return 'anime' if @@matcher.match(@url)["type"] == 'a'
-    return 'manga' if @@matcher.match(@url)["type"] == 'm'
-    @@matcher.match(@url)["type"]
+    return 'anime' if @@matcher.match(@url)['type'] == 'a'
+    return 'manga' if @@matcher.match(@url)['type'] == 'm'
+    @@matcher.match(@url)['type']
   end
 
   def slug
-    @@matcher.match(@url)["slug"]
+    @@matcher.match(@url)['slug']
   end
 
   def media
-    @_media ||= type.classify.constantize.find(slug)
-  rescue
-    nil
+    raw[type]
   end
 
   def media_type
-    if media.is_a?(Anime)
-      media.show_type
-    elsif media.is_a?(Manga)
-      media.manga_type
+    case media['type']
+      when 'anime'; media['show_type']
+      when 'manga'; media['manga_type']
     end
   end
 
