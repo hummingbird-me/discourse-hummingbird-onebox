b954e3c16907eeef7f0dfe62e88a6b4458993028
xwmario <alankrt@gmail.com>
2016-04-06 23:52:52 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -46,8 +46,8 @@ module Onebox
   private
 
   def type
-    return 'anime' if @@matcher.match(@url)['type'] == 'a'
-    return 'manga' if @@matcher.match(@url)['type'] == 'm'
+    return 'anime' if @@matcher.match(@url)['type'] == 'anime'
+    return 'manga' if @@matcher.match(@url)['type'] == 'manga'
     @@matcher.match(@url)['type']
   end
 
